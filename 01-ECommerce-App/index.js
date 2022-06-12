186203925165492f50715c810aef65e9fb7e6c59
yescine <bouchoucha.yassine@gmail.com>
2022-06-12 19:40:21 +0100

    exploring scalars

--- 01-ECommerce-App/index.js
+++ 01-ECommerce-App/index.js
@@ -3,18 +3,27 @@ const { ApolloServer, gql } = require("apollo-server")
 const typeDefs = gql`
   type Query {
     hello:String
+    noAnimals:Int
+    price:Float,
+    isAdmin:Boolean
   }
 `
 
 const resolvers = {
-  Query:{
-    hello:()=>{
-      return "hello world"
+  Query: {
+    hello: () => {
+      return "hello any"
+    },
+    noAnimals: () => {
+      return 55
+    },
+    price: () => {
+      return 99.99
     }
   }
 }
 const server = new ApolloServer({
-  typeDefs,resolvers
+  typeDefs, resolvers
 })
 
 server.listen()
