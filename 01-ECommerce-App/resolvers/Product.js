58c01e0e9496845aec1ebcd3e484cc61fac6d629
yescine <bouchoucha.yassine@gmail.com>
2022-06-12 18:11:14 +0100

    delete existing ecommerce project

--- 01-ECommerce-App/resolvers/Product.js
+++
@@ -1,8 +0,0 @@
-exports.Product = {
-  category: ({ categoryId }, args, { db }) => {
-    return db.categories.find((category) => category.id === categoryId);
-  },
-  reviews: ({ id }, args, { db }) => {
-    return db.reviews.filter((review) => review.productId === id);
-  },
-};
