0e663f6937a715eba9332db77a683db0ac7b7b9b
yescine <bouchoucha.yassine@gmail.com>
2022-06-12 21:08:37 +0100

    embeded queries

--- 01-ECommerce-App/index.js
+++ 01-ECommerce-App/index.js
@@ -1,11 +1,24 @@
 const { ApolloServer, gql } = require("apollo-server")
 
+const products = require('./data/product')
 const typeDefs = gql`
   type Query {
     hello:String
     noAnimals:Int
     price:Float,
-    isAdmin:Boolean
+    isAdmin:Boolean,
+    roles:[String!]!
+    products:[Product!]!
+    product(id:ID!):Product 
+  }
+
+  type Product {
+    id:String!
+    name: String!
+    description: String!
+    quantity:Int
+    price: Float!
+    onSale:Boolean
   }
 `
 
@@ -19,8 +32,18 @@ const resolvers = {
     },
     price: () => {
       return 99.99
+    },
+    roles: () => {
+      return ["some","data","null"]
+    },
+    products:()=>{
+      return product
+    },
+    product:(_,args,ctx)=>{
+      const product = products.find(elem=>elem.id===args.id)
+      return product?product:null
     }
-  }
+  },
 }
 const server = new ApolloServer({
   typeDefs, resolvers
